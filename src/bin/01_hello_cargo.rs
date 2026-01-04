777a677ec33d72cb00cd5315baf38fd5ab0604ab
noomcreator <rueangyottreemat@gmail.com>
2026-01-04 23:29:04 +0700

    chore: learn 01_hello_cargo.rs

---
+++ src/bin/01_hello_cargo.rs
@@ -0,0 +1,20 @@
+/* Hello World & Cargo
+
+🔑 Key Concepts : 
+    - cargo is Rust's package manager and build system.
+    - `cargo new <name>` creates a new Rust project (like this one).
+    - `cargo run` to run the project.
+    - `fn main()` is the main function, the entry point for every Rust program.
+    - `println!` is a "macro" that prints text to the console.
+*/
+
+fn main() {
+    //this is a one line comment
+    println!("Hello, world from Example 1!");
+
+    /* This is a
+       multi-line comment */
+
+    /// this is documentation comment
+    println!("Hello, world from Example 1 again!");
+}
